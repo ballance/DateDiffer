113294b135807639fcb8ae6662e8b5fb07caa187
Ballance <ballance@gmail.com>
2014-07-23 15:37:44 -0400

    first commit

---
+++ datediffer/DateDiffer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datediffer
+{
+    public class DateDiffer
+    {
+        private DateTime _endDateTime;
+        public DateDiffer(DateTime setEndDateTime)
+        {
+            _endDateTime = setEndDateTime;
+        }
+
+        public TimeSpan TimespanUntil()
+        {
+            return DateTime.Now - _endDateTime;
+        }
+
+        public int DaysUntil()
+        {
+            //if (DateTime.Now.Month != _endDateTime.Month)
+            //    return _endDateTime.Day - 1;
+            //else 
+            return _endDateTime.Day - DateTime.Now.Day;
+        }
+
+        /// <summary>
+        /// This is not trivial...
+        /// </summary>
+        /// <returns></returns>
+        public int MonthsUntil()
+        {
+            var monthses = new List<Months>();
+
+            var monthCount = 0;
+
+            var currentMonth = DateTime.Now.Month;
+            var currentYear = DateTime.Now.Year;
+                        
+            var runningMonth = DateTime.Now.Month;
+            var runningYear = DateTime.Now.Year;
+
+            foreach (var year in Enumerable.Range(currentYear, _endDateTime.Year-currentYear+1))
+            {
+                Console.WriteLine(year);
+                if ((currentYear == (_endDateTime.Year-1)))
+                {
+                    monthCount += Enumerable.Range(currentMonth, 12).Count();
+                }
+                else if (currentYear == _endDateTime.Year)
+                {
+                    monthCount += Enumerable.Range(currentMonth, _endDateTime.Month-currentMonth).Count();
+                }
+                else if (currentYear < (_endDateTime.Year - 1))
+                {
+                    monthCount += 12;
+                }
+            }
+
+            return monthCount;
+        }
+
+        public string Verify(int months, int days)
+        {
+            var diff = DateTime.Now.AddMonths(months).AddDays(days);
+            return diff.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+
+    public class Months
+    {
+        public int Year { get; set; }
+
+        public int Month { get; set; }
+    }
+}
---
+++ datediffer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datediffer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateDiffer = new DateDiffer(new DateTime(2014, 7, 24));
+
+            var monthsUntil = dateDiffer.MonthsUntil();
+            var daysUntil = dateDiffer.DaysUntil();
+
+            Console.WriteLine("{0} months, {1} days", monthsUntil, daysUntil);
+
+            Console.WriteLine("verification: {0}", dateDiffer.Verify(monthsUntil,daysUntil));
+            Console.ReadKey();
+        }
+    }
+}
